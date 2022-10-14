329a2bfa59ab42a3b6d90cc7ae4e24ac7b6cb2a0
Danyos <daniel.hambardzumyan@mail.ru>
2022-10-14 17:22:49 +0400

    ok

--- js-1/script.js
+++ js-1/script.js
@@ -1,15 +1,29 @@
-x=7
-y=5
-c=0
+// x=7
+// y=5
+// c=0
 
-c=x//7
-x=y //5
-y=c //7
+// c=x//7
+// x=y //5
+// y=c //7
 
-console.log("x",x)
-console.log("y",y)
+// console.log("x",x)
+// console.log("y",y)
 
 
-// 
-a=10
-b=30
+// // 
+// a=100
+// b=300
+// a=b+a//40
+// b=a-b//10
+// a=a-b//30
+x=1
+y=2
+z=5
+
+console.log(1+2*5)
+x=x+1 //2
+y=7 //7
+console.log(x+y*z)
+
+
+
