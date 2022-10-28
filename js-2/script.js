a60d2cc398a7179956121dc2ad8b3940b30514c9
Danyos <daniel.hambardzumyan@mail.ru>
2022-10-28 20:47:27 +0400

    Das-2

---
+++ js-2/script.js
@@ -0,0 +1,190 @@
+// let x
+
+// x=8
+// let y=x //8
+// y+=x //16
+// x=y-x//8
+// // y%=2
+// // y=x+y==16
+// let z="barev vonces"
+// // z+=y
+// let w=z+" Dav jan inch ka"
+// let t=""+y
+// // string+number=stirng
+
+// console.log(w,y)
+
+// let names="Samo"
+// let surname="Poghosyan"
+// let usename;
+// let age=22
+
+// usename=names+" "+surname+ 
+// " jan du vaxy kdarnas "+age+"
+// tarekan"
+// usename=`vaxy mer ${names} ${surname} kdarna ${age} tarekan`
+// console.log(usename)
+
+// let x="1000"
+// let idram=5000;
+// idram=idram + +x
+// idram=idram + parseFloat(x)
+
+
+// console.log(idram)
+
+// String " "
+// Number 55+5
+// Boolean //true false
+// null datark
+// undefined chgtnvac arjeq
+// Object {}
+// 5===5// true
+
+// 5==5
+// true
+
+// 5=="5"
+// true
+
+// 5==="5"
+// false
+
+// 5!=5
+// false
+
+// !false
+// true
+
+// !true
+// false
+
+// !!5
+// true
+
+// !!1
+// true
+
+// !!0
+// false
+
+// !!null
+// false
+
+// !!Object
+// true
+
+// !!""
+// false
+
+// !!undefined
+// false
+
+
+﻿
+// undefined==null
+// true
+// undefined===undefined
+// true
+// null===null
+// true
+// 0===0
+// true
+// 0===-0
+// true
+// 0.1+0.1
+// 0.2
+// 0.1+0.2
+// 0.30000000000000004
+
+
+// let x=77
+// let str="df;vgbp"
+// x=x-str
+// console.log(x)
+// NaN
+// typeof NaN
+// 'number'
+// NaN===NaN
+// false
+// 333333*333
+// 110999889
+// 333333**333
+// Infinity
+// Infinity===Infinity
+// true
+// +"123456asdasd"
+// NaN
+// +"1a"
+// NaN
+// parseInt("a112345")
+// NaN
+// parseInt("1123a45")
+// 1123
+// "sdcns sd cf" + 55
+// 'sdcns sd cf55'
+// "sdcns sd cf" % 55
+// NaN
+
+// !undefined
+// true
+
+// !NaN
+// true
+
+// +"2d"===NaN
+// false
+
+// NaN===NaN
+// false
+
+// let age=prompt("Greq dher tariqy")
+// console.log(+age > 0 )
+// console.log(!isNaN(age))
+// console.log(!Object.is(NaN,+age));
+// true
+// 7>5 && true
+// true
+// 7>5 && 7<13 && 1>5
+// false
+// 7<5 || 5===5 || 7>5
+// true
+// 7>5 && 8==7 || 7==9 || 5===5
+// true
+
+
+// false arjeqner
+
+// 1.false
+// 2. 0
+// 3. underfined
+// 4. null
+// 5. ""
+// 6.NaN
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://learn.javascript.ru/type-conversions
+// https://learn.javascript.ru/comparison
+
+// https://learn.javascript.ru/ifelse
+
+// stexcel mi aynpisi payman vor marun asenq mutqagri qo tariqy (prompt()) 
+// 1> mutqagrac tivy erb 18 ic poqr lini asi duq che kaox mutq gortcel qani vor anchapahas eq
+// 2> erb mardy gri 18 ic minchev 63 @nkac mijakyqum aseq shnorhavorum enq duq barehajox gancveciq
+// 3> 63 ic mets grelu depqum asel duq tariqov shat mets eq
+
+
+// 4 paymaner ee mardy griq hankarcaki sxal artahytutyun orinak "151d5fvd1" inchvor senc arthaytutyun vor karox e hangecnel nani aseq xndrum enq grel dher tariqy chisht
+
+
