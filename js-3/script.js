dd901e170df40abe0c510bfeb798353aae4db240
Danyos <daniel.hambardzumyan@mail.ru>
2022-10-31 20:53:32 +0400

    das-3

---
+++ js-3/script.js
@@ -0,0 +1,40 @@
+// let tiv_1=+prompt("xndrum enq grel tiv") // abc
+// let tiv_2=+prompt("xndrum enq grel tiv 2") //123
+// let result  =  0
+// if(!isNaN(tiv_1) && !isNaN(tiv_2)){
+//     let gortcoxutyun=prompt("xndrum enq grel gortcoxtyan nshan@")
+//     if(gortcoxutyun==='+'){
+//         result=tiv_1+tiv_2
+//     }else if(gortcoxutyun==='-'){
+//         result=tiv_1-tiv_2
+//     }else if(gortcoxutyun==='/'){
+//         result=tiv_1/tiv_2 
+//     }
+//     else if(gortcoxutyun==='*'){
+//         result=tiv_1*tiv_2
+//     }
+// }
+// if(result){
+//     console.log(result*2)
+// }else{
+//     console.log('en chi')
+// }
+
+let tiv=+prompt()
+
+if(tiv%2===0){
+    console.log('kent chi');
+}else{
+    console.log('kent');
+}
+
+
+// 1. promptov tiv grel tvyal tivy lini drakan te bacasakan veradarcni drakan
+//2. a=7 b=21 =14 gtnel mec ev poqr vrery
+// 3. unenq defoult login="user123" psw="123456" 
+// 3.1 petq e stugel dashtery miji andramaner "length"
+// 3.2 ete ogtatery gri ir login USER123 naev metsatar asi chisht e
+// 3.3 ete chisht e asum e ok ete che sxal tvyalner
+
+// ognakan
+// https://www.w3schools.com/js/js_string_methods.asp
