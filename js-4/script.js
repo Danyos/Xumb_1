2c8a18a1a29a2c5c9e165a0aebeb9af0525ad1eb
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-02 20:58:38 +0400

    das  4 ternar operator

---
+++ js-4/script.js
@@ -0,0 +1,117 @@
+// let x=+prompt("greq tiv")
+
+// if(x>0){
+//     console.log(x)
+// }else{
+//     console.log(x*-1)
+// }
+
+
+// let login_user="Sam123"
+// let password_user="12345678"
+// let login=prompt("greq login@",'Sam123')
+// let psw=prompt("greq passwordy",'12345678')
+
+// if(login.length>5 && psw.length>=7){
+//     if(login_user.toLowerCase()===login.toLowerCase() && password_user===psw){
+//         console.log("ok")
+//     }else{
+//         console.log("sxal tvyalner")
+//     }
+// }else{
+//     console.log("qich eq grel ")
+// }
+
+// let x="Barev"
+// switch(x){
+//     case "hajox":
+//         console.log("hajox")
+//         break
+//     case "Barev":
+//         console.log("barev")
+//         break
+//     case "Lav mna":
+//         console.log("lav mna")
+//         break
+//     default:console.log("voch mek")
+// }
+
+
+let y=2
+let res=null
+let result=null
+
+// if(y== 1){
+//     res="pink"
+// }else if(y==2 || y==20){
+//     res="green"
+// }
+// else{
+//     res="red"
+// }
+
+ result=y==1?"pink":y==2 ?"green": "red" 
+
+
+
+console.log(result)
+// let result2=false;
+
+
+// let chek=confirm("Gnan te mnan")
+// if(chek){
+//     result2="green"
+// }else{
+//     result2="red"
+// }
+// versia 1
+// document.write(`<h1 style=color:${result2}>Bai gisher</h1>`)
+
+// versia 2
+// let chek=confirm("Gnan te mnan")
+
+// document.write(`<h1 style=color:${chek?"green":"red"}>Bai gisher</h1>`)
+
+// tnayin cody ksarqeq ternar operatorov
+// 1 xndir
+// let a=7
+// let b=15
+// let c=25
+// gtnel amenamecy
+
+// 2 xndir
+// let message;
+
+// if (login == 'garik23' && psw == 'Valodya23') {
+//   message = 'Привет';
+// } else if (login == 'Samvel123' && psw == 'Muxi123') {
+//   message = 'Здравствуйте';
+// } else if (login == '' && psw ==') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+
+// 3 rd xndir
+
+// let result;
+
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
