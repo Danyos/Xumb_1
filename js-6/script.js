d98fba28fadee0c2e7b6e5b58d9135f21b1d8ac5
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-07 20:56:47 +0400

    Operators Math

---
+++ js-6/script.js
@@ -0,0 +1,125 @@
+// tnayin
+
+// valod>Valod
+// VaLod>vaLod
+// 1 prompt unenal stugel ete arajin tary metsatar 
+// e sarqi poqratar miayn arajin tary nuynel kakaraky ete 
+// arajin tary poqratar e sarqi metsatar
+
+
+// 2. Stugel ete anvan mej
+//  ka - nshan tox inqy vecni dranic heto artahaytutyuny
+
+// 3. ogtvoxic uzeq anun ev anuny texadreq texti mej u aynopes
+//  areq vor artahyutyan mej misht ir anun@ avtomat sheshtvi
+
+// 4. unenal 2 prompt vori mej kgres tiv(index) te vor ketic es cankanum
+//  avelacnel texty 2 prompti mej texty te inch es uzum avelana ayd ketic
+
+
+// let index=prompt("xndrum enq grel te vor indexic eq canknum sksel ")
+// let x="Barev im sireli hayrenakic"
+// let y=x.slice(0,index)
+// let z=x.slice(index)
+// let res=`${y} Husoven laveq  ${z} `
+
+// console.log(res)
+
+// let names=prompt("xndrum enq grel anun@")
+// let speak="Voghjuyn FullName jan, duq grancvel eq, maskacelu hamergin, FullName jan xndrum enq 19:00 motenal mer grasenyak, FullName jan xnndrum enqn dher het vercnel andnagir"
+// let y=speak.replaceAll("FullName",names)
+// console.log(y)
+  
+// let names="Grisha"
+// if(names[0]===names[0].toLowerCase()){
+//     names=names[0].toUpperCase()+names.slice(1)
+// }else{
+//     names=names[0].toLowerCase()+names.slice(1)
+// }
+// 
+// onsole.log(names)
+
+// let str="ipsum dolor sit, lorem amet consectetur adipisicing elit. lorem Laboriosam sunt eligendi at magni soluta eveniet, lorem  ex nulla. Vitae, molestiae ullam! Possimus, asperiores excepturi? lorem Aspernatur necessitatibus quos ut vel consequatur lorem aperiam."
+
+// let x=str.indexOf("lorem")// 55
+// let y=str.slice(0, x+6)+ str.slice(x+6).replaceAll("lorem",'')
+
+// console.log(y)
+
+
+// let x=7
+
+// x=!x?"barev":x
+// x??="-"
+
+// ||
+// x||=7
+
+// &&
+// x&&=false||25 || 99
+
+// document.write(x??"datark")
+
+
+
+// 1.
+let x=Math.round(Math.random()*33)
+// 0,0.999999
+console.log(x)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+// Math.E
+// Math.LN10
+// Math.LN2
+// Math.LOG10E
+// Math.LOG2E
+// Math.PI
+// Math.SQRT1_2
+// Math.SQRT2
+// Methods
+// Math.abs()
+// Math.acos()
+// Math.acosh()
+// Math.asin()
+// Math.asinh()
+// Math.atan()
+// Math.atan2()
+// Math.atanh()
+// Math.cbrt()
+// Math.ceil()
+// Math.clz32()
+// Math.cos()
+// Math.cosh()
+// Math.exp()
+// Math.expm1()
+// Math.floor()
+// Math.fround()
+// Math.hypot()
+// Math.imul()
+// Math.log()
+// Math.log10()
+// Math.log1p()
+// Math.log2()
+// Math.max()
+// Math.min()
+// Math.pow()
+// Math.random()
+// Math.round()
+// Math.sign()
+// Math.sin()
+// Math.sinh()
+// Math.sqrt()
+// Math.tan()
+// Math.tanh()
+// Math.trunc()
+// nayel bolor@
+
+
+
+
+
+
+
+
+
+
+
