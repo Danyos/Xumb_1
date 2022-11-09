d86084f195e0ff8209444a5ade17a7d6445450da
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-09 21:02:56 +0400

    For clik xndirner

---
+++ js-7/script.js
@@ -0,0 +1,50 @@
+
+
+
+
+// let anun="Anun"
+// for(let i=0;i<anun.length;i++){
+//     console.log(anun[i])
+// }
+
+// 1. tpel kent tvery
+// let res=0
+for(let i=0;i<21;i++){
+
+    if(i%2==0){
+        continue
+    }
+    console.log(i)
+}
+
+
+
+// 2. tpel ayn tvery voronq bajnvum en 7 ev 5 i 
+
+
+// 3. tpel ayn tvery vor tvyal tvi ev naxord tveri gumary zuyg che 
+
+// 4. tpel minchev 1000@ chgerazancox tvi qarakusin
+
+// 5. unenal 2 prompt vortex 1 promptum kgres te vor tvic sksi 
+//   2 rd promptum vortex avartvi ayd hatvacum ynkac tvery tpel
+        // 5.1 hashvel dranc aranq tveri gumary
+        // 5.2 hashvel zuyg tveri gumary
+        // 5.3 tpel ayn tvery vor tvyal tvi ev naxord tveri gumary zuyg che
+
+//  tnayin
+
+// 1.unenal 2 prompt vori mej kgres tivy 2 rd prompti mej astichan@
+//  aranc ** ogtagorcman hashvel tvi qarakusin
+// 2. unenal prompt vori mej tiv greluc khashvi factoryal@ orinak. mut exac tivy 6 e veradarcni
+// 1*2*3*4*5*6
+// 3. stexcel aynpisi cikl vor kashxati hakarak orinak sovorakan devy 1,2,3,4,5,6,7,8,9.... 
+// mez petq e tpel 9,8,7,6,5,4,3,2,1
+// 4. unenal prompt vori mej gres anun shur ta hakarak ogtagortcel cikl
+// 5. unenal prompt harcnel anun@ parzel ete hayatar grvac che kam baci hayereniv ayl ban gtnvum e asel sxal hakarak depqum ok
+// orgvel  charCodeAt() https://www.w3schools.com/js/tryit.asp?filename=tryjs_string_charcodeat
+
+
+
+
+
