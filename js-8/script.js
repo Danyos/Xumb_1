38619d32d421a57fe59f672a7751b87148318da1
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-11 20:53:58 +0400

    Nerkarucvox cikl

---
+++ js-8/script.js
@@ -0,0 +1,153 @@
+
+
+
+
+// let anun="Anun"
+// for(let i=0;i<anun.length;i++){
+//     console.log(anun[i])
+// }
+
+// 1. tpel kent tvery
+// let res=0
+// for(let i=0;i<21;i++){
+
+//     if(i%2==0){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+
+// 2. tpel ayn tvery voronq bajnvum en 7 ev 5 i 
+
+
+// 3. tpel ayn tvery vor tvyal tvi ev naxord tveri gumary zuyg che 
+
+// 4. tpel minchev 1000@ chgerazancox tvi qarakusin
+
+// 5. unenal 2 prompt vortex 1 promptum kgres te vor tvic sksi 
+//   2 rd promptum vortex avartvi ayd hatvacum ynkac tvery tpel
+        // 5.1 hashvel dranc aranq tveri gumary
+        // 5.2 hashvel zuyg tveri gumary
+        // 5.3 tpel ayn tvery vor tvyal tvi ev naxord tveri gumary zuyg che
+
+//  tnayin
+
+// 1.unenal 2 prompt vori mej kgres tivy 2 rd prompti mej astichan@
+//  aranc ** ogtagorcman hashvel tvi qarakusin
+//Samo
+// 2. unenal prompt vori mej tiv greluc khashvi factoryal@ orinak. mut exac tivy 6 e veradarcni
+// 1*2*3*4*5*6
+// Artyom, Ando,Samo
+// 3. stexcel aynpisi cikl vor kashxati hakarak orinak sovorakan devy 1,2,3,4,5,6,7,8,9.... 
+// mez petq e tpel 9,8,7,6,5,4,3,2,1
+// Artyom,Ando, Samo
+// 4. unenal prompt vori mej gres anun shur ta hakarak ogtagortcel cikl
+// ando, sam
+// 5. unenal prompt harcnel anun@ parzel ete hayatar grvac che kam baci hayereniv ayl ban gtnvum e asel sxal hakarak depqum ok
+// orgvel  charCodeAt() https://www.w3schools.com/js/tryit.asp?filename=tryjs_string_charcodeat
+
+
+// 1329*1414
+
+
+
+// let anun=prompt("Greq hayeren anun")
+// let anun_status=false
+// for(let i=0;i<anun.length;i++){
+//     if(anun.charCodeAt(i)>=1329 && anun.charCodeAt(i)<=1414){
+//         anun_status=true
+//     }else{
+//         anun_status=false
+//         break
+//     }
+// }
+// console.log(anun_status)
+
+
+
+
+
+
+
+// for(let i=0;i<100;i++){
+//     if(i**2<=1000){
+//         console.log(i)
+//     }else{
+//         break
+//     }
+// }
+
+
+
+
+// mardy gri mijakayq HashChangeEventl ayd tvi amenames tivi qarkusin
+// 1000
+// 10
+// 50000000
+// for(let i=0;true;i++){
+
+// if(i**2<=10000){
+//     console.log(i)
+// }else{
+//     break
+// }
+// }
+
+
+
+
+
+
+
+// let a=0
+// while(true){
+// a++
+// if(a**2<=10000){
+//     console.log(a)
+// }else{
+//     break
+// }
+// }
+
+
+
+// document.write("<div class='math'>")
+// for(let i=2;i<=9;i++){
+//     document.write("<div>")
+//     for(let j=1;j<=9;j++){
+   
+//         document.write(i+"x"+j+"="+i*j)
+//         document.write("<br>")
+//     }
+//     document.write("</div>")
+// }
+// document.write("</div>")
+
+
+
+
+// tnayin 1
+document.write("<table class='chees' border='0'>")
+for(let i=0;i<8;i++){
+    document.write("<tr>")
+    for(let j=0;j<8;j++){
+        document.write("<td>")
+        document.write("</td>")
+    }
+    document.write("</tr>")
+}  
+  document.write("</table>")
+
+// tnayin 2
+// unenal prompt vori mej grelov mijakayq khashvi tvyal mijakayqi parz tver@
+
+
+// tnayin 3
+
+// nakri tesqov tnayin.jpg
+
+        
+
+
