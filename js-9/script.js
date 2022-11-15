72d33c95bad86f52be896890c68cda313b04b05e
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-15 14:24:03 +0400

    for string xndirner

---
+++ js-9/script.js
@@ -0,0 +1,61 @@
+// let n=8
+// for (let i = 1; i <= n; i++) {
+//   let str=' '.repeat(n-i);
+//   let str2='*'. repeat(i * 2 -1);
+
+//   console.log(str + str2 + str);
+
+  
+// }
+
+
+// for(let i=1; i<= n; i++){
+
+//   let str = ' '.repeat(n-i);
+
+//   let str2 = '*'. repeat(i * 2 -1);
+
+
+
+//   console.log(str + str2 + str);
+
+// }
+
+  //1 shaxmati daska unenal prompt vot gres orinak stexci aydpes 10,8  
+// let table=prompt("greq tivy ays formatov 8,8")// "10,5"
+
+// let y=table.slice(0, table.indexOf(","))
+// let z=table.slice(table.indexOf(",")+1)
+
+// document.write("<table class='chees' border='0'>")
+// for(let i=0;i<y;i++){
+//     document.write("<tr>")
+//     for(let j=0;j<z;j++){
+//         document.write(`<td style=background:${(j+i)%2==0?'red':'green'};>`)
+//         document.write("</td>")
+//     }
+//     document.write("</tr>")
+// }  
+//   document.write("</table>")
+
+
+
+// let text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel voluptatibus necessitatibus quaerat autem soluta sed nihil, possimus fuga aspernatur est, perferendis nobis id amet a aliquid non expedita dignissimos sint."
+// let newtext=text.slice(0,text.indexOf(".")+1)
+// console.log(newtext)
+
+
+
+// tnayin
+
+
+// 2. prompti mej tiv gres tvery gumari irar
+
+// 3.  promptum , arandznacnel tver inqy gtni ayd tveri vra bajanvox tver minchev 1000 mijayq (50,20,25)
+
+
+
+
+
+
+
