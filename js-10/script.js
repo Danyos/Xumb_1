125b609f21fcedc7ee163da8b6aa27f8540aca17
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-16 20:46:47 +0400

    array method and string method

---
+++ js-10/script.js
@@ -0,0 +1,142 @@
+// 2. prompti mej tiv gres tvery gumari irar
+// ete tvi mej lin orinak 9@ ev 0 chgumares
+
+
+// let x=5894
+// let y=x.toString()
+// let res=0
+// for(let i=0;i<y.length;i++){
+//  if(y[i]==0 || y[i]==9){continue}
+//   res+=+y[i]
+// }
+
+// console.log(res)
+// let str="Barev bolorin, vonceq laveq, mish lav exeq inchqam, tesnem lav tesnem"
+// let x=str.replaceAll(",","").replaceAll(" ", "_")
+// console.log(x)// result Barev-bolorin-vonceq-laveq-mish-lav-exeq-inchqam-tesnem-lav-tesnem
+
+
+
+
+// let str="Barev bolorin, 25 vonceq laveq, 44 34 44 55 77 misht lav exeq inchqam, 22 tesnem 88 lav tesnem"
+
+// xndir
+// 1. tvery gumarel 
+// 2. xndiry hanel tvery
+
+// 1.
+// let arr=str.split(" ")//['Barev', 'bolorin,', 'vonceq', 'laveq,', 'misht', 'lav', 'exeq', 'inchqam,', 'tesnem', 'lav', 'tesnem']
+// arr.slice(1,15) // ktrum er vortexic vortex erb ches asum vortex vercnum e tvyal ketic sax...(himnakan zangvacy chi poxum)
+// arr.push(7) //verjic avelacnum e
+// arr.unshif(8) //skzbic avelacnum e
+// arr.indexOf // gtnum er indy
+// arr.pop() // ktrum e verjic 
+// arr.shift() // skzbic
+// arr.reverse() //shur e talis hakarak
+// arr.splice(arr.indexOf("inchqam,"),1)
+// arr.splice(5,1)// ktrum e vor indexic qani hat baci da hnaravor e avelacnel 3 arjeq pooxareny texadrelu hamar, bayc texadrum e miayn meky
+// for(let i=0;i<arr.length;i++){
+//   if(!isNaN(arr[i])){
+//     arr.splice(arr.indexOf(arr[i]),1)
+//     i-=1
+//   }
+// }
+// str=arr.join(" ")
+
+// console.log(arr)
+
+
+// tnayin
+// [1,"ergfe3tr",'true',true,false,-78,58,4,54,5,"5652","barev"] 
+// 1. zasngvaci unenq zangvac //veradarcnel maqur tverov zangvac 
+// 2. hashvel tveri gumary
+// 3. very verapoxer **2
+// *******************
+// [45,7,8,9,4,2,8,56,5,4,2,57,55,7,7]
+// 1.stanal xary tverov zangvac minchev 100 mijakayqum
+// 2, maqrel zangvacy krknvoxneric
+// 3. aradznacnel tvyal zanvaci zuygery ev kentery even ev odd zangvacnerum
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3.  promptum , arandznacnel tver inqy gtni ayd tveri vra bajanvox tver minchev 1000 mijayq (50,20,25)
+
+
+
+
+
+
+
+
+
+
