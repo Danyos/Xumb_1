f0b8fdc36af8e21c1a2e8d5b078fa3bb50f21128
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-18 20:20:27 +0400

    zangvac xndirner

---
+++ js-11/script.js
@@ -0,0 +1,97 @@
+// xndir 
+
+// 1. drakan tareri mijin tvabanakan@
+// 2.1. zuyg index unecox tareri gumary
+// 2.2. zuyg index unecox tareri artadryal@
+// 2.3. kent tar unecox indexneri gumary
+// 3. drakan ev bacasakan tarri qanak@
+// 4. gtnel ayn tary vor ir indexi vra kbajanvi anmnacord
+// 5. gtnel ayn tary vor ir indexy mets e ayd tvic
+// 6. gtnel ayn tareri qanaky vor indexy mets e tvyal x tvic veradarcnel nor zagvaci tesqov
+// 7.gtnel zagvaci metsaguyn tary
+// 8. gtnel zagvaci poqraguyn tary
+// 9.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3.  promptum , arandznacnel tver inqy gtni ayd tveri vra bajanvox tver minchev 1000 mijayq (50,20,25)
+
+
+
+
+
+
+
+
+
+
