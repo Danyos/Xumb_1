59b27c49be09371653582cd181d7223190efbbbf
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-18 20:33:22 +0400

    Xndir zangvacneric

--- js-11/script.js
+++ js-11/script.js
@@ -1,16 +1,19 @@
-// xndir 
+// xndir tnayin
 
-// 1. drakan tareri mijin tvabanakan@
-// 2.1. zuyg index unecox tareri gumary
+// 1. drakan tareri mijin tvabanakan@ //[1,5,-1,7,-5,-4,9,8,7,2,-10], tveri gumary bajanac qanaki
+// 2.1. zuyg index unecox tareri gumary 
 // 2.2. zuyg index unecox tareri artadryal@
 // 2.3. kent tar unecox indexneri gumary
-// 3. drakan ev bacasakan tarri qanak@
-// 4. gtnel ayn tary vor ir indexi vra kbajanvi anmnacord
-// 5. gtnel ayn tary vor ir indexy mets e ayd tvic
-// 6. gtnel ayn tareri qanaky vor indexy mets e tvyal x tvic veradarcnel nor zagvaci tesqov
-// 7.gtnel zagvaci metsaguyn tary
+// 3. drakan ev bacasakan tarri qanak@ //[1,5,-1,7,-5,-4,9,8,7,2,-10]  //7//4
+// 4. gtnel ayn tary vor ir indexi vra kbajanvi anmnacord [1,5,4,15,-5,-4,9,8,7,2,-10]// [4,15]
+// 5. gtnel ayn tareri qanaky vor indexy mets e tvyal x tvic veradarcnel nor zagvaci tesqov
+// 6.gtnel zagvaci metsaguyn tary
+// 7. zangvaci 2 rd amenaets arjeqy
 // 8. gtnel zagvaci poqraguyn tary
-// 9.
+// 9. gtnel zagvaci mej ayn tarery 
+// voronq chunen ayd zangvaci mej voreve andam vor kbajanvi ir vra anmnacord
+
+
 
 
 
