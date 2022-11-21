30818eb141750d746b513da06dcca37e05eab6f0
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-21 21:02:21 +0400

    Array xnidrner -2

---
+++ js-12/script.js
@@ -0,0 +1,118 @@
+//  gtnel zagvaci mej ayn tarery 
+// voronq unen ayd zangvaci mej ir vra bajanvox andam baci irencic
+
+
+// let arr=[1,5,7,8,9,10,4,12,14,16,18,20,5,4,3,2]
+// let thisArr=[]
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr.length;j++){
+//         if(i!=j && arr[i]%arr[j]==0 && arr[j]>1){
+//             thisArr.push(arr[i])
+//             break
+
+//         }
+//     }
+// }
+// console.log(thisArr)
+
+// unenq 2 zangvac [1,5,7,8,9,10,4,12,14,16,18,20,5,4,3,2] and [10,4,12,1]
+// veardarcnenq ayn anadmenry vornq chkan 2 rd zangvacum
+
+
+
+
+// tnayin
+
+// 1. unenal zangvac ["100$","500$","250$","1000$","750$"] hashvel irenc gumary
+// 2. aranc push hramani ogtagortcman avelacnel arjeq verjum
+// 3. aranc unshift hramani ogtagortcman avelacnel arjeq skzbum
+// unenq parz tver [7,8,9,1,5,4,7,6,3,2] aranc sort hramani dasavorel achman kargov
+// 4. aranc unshift hramani ogtagortcman avelacnel arjeq skzbum
+// unenq parz tver [7,8,9,1,5,4,7,6,3,2] aranc sort hramani dasavorel nvazman kargov
+// 5. [NaN,7,"7","Barev",NaN,null,55,25,45,"200$","200.9$","42.5$",70.7] veradarcnel maqur zangvac 
+    // 5.1 vercnel tvery kap chuni string e te number
+    // 5.2 gumarel tvery
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3.  promptum , arandznacnel tver inqy gtni ayd tveri vra bajanvox tver minchev 1000 mijayq (50,20,25)
+
+
+
+
+
+
+
+
+
+
