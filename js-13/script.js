b409581031dd1f8008bde7f9b5de3a109c15df6d
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-23 20:42:44 +0400

    arr for in of

---
+++ js-13/script.js
@@ -0,0 +1,169 @@
+// . [NaN,7,"7","Barev",NaN,null,55,25,45,"200$","200.9$","42.5$",70.7] veradarcnel maqur zangvac 
+    // 5.1 vercnel tvery kap chuni string e te number
+    // 5.2 gumarel tvery
+
+// let arr = [NaN,7,"7","Barev",NaN,null,55,25,45,"200$","200.9$","42.5$",70.7]
+// let newArr = []
+// let sum = 0
+// for(let i = 0; i < arr.length; i++) {
+   
+//     if(!Object.is(parseFloat(arr[i]), NaN)) {
+//         newArr.push(parseFloat(arr[i]))
+//         sum += parseFloat(arr[i])
+//     }
+// }
+// console.log(newArr);
+// console.log(sum);
+
+//  aranc unshift hramani ogtagortcman avelacnel arjeq skzbum
+
+// let arr=[77,2,3,47,58,74,75,98]
+// arr.reverse()
+// arr[arr.length]=arr[arr.length-1]+1
+// arr.reverse()
+// console.log(arr)
+
+// let arr=[77,2,"3",null,undefined,74,75,98]
+
+
+// in staun enq index
+// of staum enq arjeqy
+
+// for(let res in arr){
+// console.log(arr[res])
+// }
+// vs
+// for(let res=0;res<arr.length;res++){
+
+// }
+// let arr2=[51,14,154,54,54,true,"sdf",56,5,14,7]
+
+
+
+
+// arr2.entries()//[[0,51],[1,14],[2,154]...]
+// let [x,y,z]=[10,20,12]
+// console.log(z)
+// for(let [index,res] of arr2.entries()){
+//     console.log(index,res)
+// }
+
+// let totn_iterator = arr2.entries();
+// for(let res=0;res<arr2.length;res++){
+//     console.log(totn_iterator.next().value);
+// }
+
+// spreed opertor
+// let x=["Daniel",...arr2]
+
+// console.log(x)
+
+
+// colon
+
+// let arr3=[51,14,154,54,54,true,"sdf",56,5,14,7]
+
+// let arr4=[...arr3]
+// arr4.push("Valod")
+// console.log(arr3)
+// console.log(arr4)
+
+// vs
+
+// let str="Merjuan jan"
+// let str2=str
+// str2+="inchka chka"
+// console.log(str)
+
+
+
+
+
+// let arr4=[51,[14,[154,[54,514]]],true,"sdf",56,5,14,7]
+// let arr5=[...arr4]
+// console.log(arr4.flat(Infinity))
+
+// tnayin
+let x=[
+    [0,0,0],//0,
+    [0,0,0],
+    [0,0,0],
+]
+// 0.1,0.2,0.3
+// 1.0,1.1,1.2
+// 2.0,2.1,2.2
+// 0.1,1.0,2.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3.  promptum , arandznacnel tver inqy gtni ayd tveri vra bajanvox tver minchev 1000 mijayq (50,20,25)
+
+
+
+
+
+
+
+
+
+
