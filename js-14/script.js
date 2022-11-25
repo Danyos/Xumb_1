3dfaa4df560de631bffa18489131a9cbaa8a8377
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-25 21:12:32 +0400

    Funcion and xo

---
+++ js-14/script.js
@@ -0,0 +1,185 @@
+
+// tnayin
+// let x=[
+//     [1,2,3],//0,
+//     [4,5,6],
+//     [7,8,9],
+// ]
+
+// let y=[...x]
+// let z=[...x]//[1,2,3,4,5,6,7,8,9]
+// z=z.flat(Infinity)
+// console.log(x[parseInt(z.indexOf(9)/3)][x[parseInt(z.indexOf(9)/3)].indexOf(9)]="x")
+// console.log(x[1][1]="x")
+// console.log(x)
+// y=x.flat(Infinity)
+
+// y.splice(Math.round(Math.random()*0y.length),1)
+
+// console.log(y)
+// let palyerStatus=1
+// for(let i=0;i<10;i){
+//      if(palyerStatus){
+//         // let palyer=+prompt(`greq ays tvereric cankacac@ ${y.join(" ")}`)
+//         // y.splice(y.indexOf(palyer),1)
+//         // x[parseInt(z.indexOf(palyer)/3)][x[parseInt(z.indexOf(palyer)/3)].indexOf(palyer)]="x"
+//         // console.log("es")
+//         let r= y.splice(y.indexOf(Math.round(Math.random()*y.length)),1)
+//         x[parseInt(z.indexOf(...r)/3)][x[parseInt(z.indexOf(...r)/3)].indexOf(...r)]="x"
+//         palyerStatus=0
+//     }else{
+//        let r= y.splice(y.indexOf(Math.round(Math.random()*y.length)),1)
+//         x[parseInt(z.indexOf(...r)/3)][x[parseInt(z.indexOf(...r)/3)].indexOf(...r)]="y"
+//         palyerStatus=1
+//     }
+//     console.log(x)
+
+//    if(y.length==0){
+//     break
+//    }
+   
+// }
+
+// 0.1,0.2,0.3
+// 1.0,1.1,1.2
+// 2.0,2.1,2.2
+// 0.1,1.0,2.0
+
+// function teman
+
+// let palyerStatus=1
+// function xo(a,b){
+
+//     if(palyerStatus){
+//     //    let palyer=+prompt(`greq ays tvereric cankacac@ ${y.join(" ")}`)
+//        y.splice(y.indexOf(b),1)
+//        x[a][b]="x"
+//        // console.log("es")
+//     //    let r= y.splice(y.indexOf(Math.round(Math.random()*y.length)),1)
+//     //    x[parseInt(z.indexOf(...r)/3)][x[parseInt(z.indexOf(...r)/3)].indexOf(...r)]="x"
+//        palyerStatus=0
+//    }else{
+//       let r= y.splice(y.indexOf(Math.round(Math.random()*y.length)),1)
+//        x[parseInt(z.indexOf(...r)/3)][x[parseInt(z.indexOf(...r)/3)].indexOf(...r)]="y"
+//        palyerStatus=1
+//    }
+//    console.log(x)
+  
+// }
+
+
+
+// function myalert(arg='Datark'){
+   
+//     document.write(arg)
+// }
+// myalert()
+
+// confirm()
+
+// let xx=7
+// function a(){
+
+// console.log(xx);
+
+// }
+// // ...
+// a()
+
+
+// // let yy=78
+// function b(arg){
+//     // ...
+//     console.log(arg%2)
+// }
+
+// // b(yy)
+// // b(48)
+
+
+// function c(){
+//     // ...
+//     b(74)
+// }
+// c();
+// let price=[100,200,300,400,500,600]
+// function view(myprice){
+//     for(let xp of myprice){
+//         console.log(xp+20)
+//     }
+// }
+
+// view(price)
+// function filterprice(){
+//     let newprice=[]
+//     for(let xp of price){
+//         if(xp>299){
+//             newprice.push(xp)
+//         }
+       
+//     }
+//     view(newprice)
+// }
+
+
+// tnayin 
+// incriment decriment + - arjeqy tpi consolum 
+// 2 x \ 0 avartel
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3.  promptum , arandznacnel tver inqy gtni ayd tveri vra bajanvox tver minchev 1000 mijayq (50,20,25)
+
+
+
+
+
+
+
+
+
+
