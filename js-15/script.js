8c8694b5dc15887bf873322f5eec611669ce11ba
Danyos <daniel.hambardzumyan@mail.ru>
2022-11-28 21:03:48 +0400

    Function expressions Declaration return

---
+++ js-15/script.js
@@ -0,0 +1,173 @@
+
+// tnayin
+// let x=[
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9],
+// ];
+//
+// let y=[...x]
+// let z=[...x]//[1,2,3,4,5,6,7,8,9]
+// z=z.flat(Infinity)
+// y=x.flat(Infinity)
+//
+// let palyerStatus=1
+// for(let i=0;i<11;i){
+//      if(palyerStatus){
+//         // let palyer=+prompt(`greq ays tvereric cankacac@ ${y.join(" ")}`)
+//         // y.splice(y.indexOf(palyer),1)
+//         // x[parseInt(z.indexOf(palyer)/3)][x[parseInt(z.indexOf(palyer)/3)].indexOf(palyer)]="x"
+//         // console.log("es")
+//         let r= y.splice(y.indexOf(Math.round(Math.random()*y.length)),1)
+//         x[parseInt(z.indexOf(...r)/3)][x[parseInt(z.indexOf(...r)/3)].indexOf(...r)]="x"
+//         palyerStatus=0
+//     }else{
+//        let r= y.splice(y.indexOf(Math.round(Math.random()*y.length)),1)
+//         x[parseInt(z.indexOf(...r)/3)][x[parseInt(z.indexOf(...r)/3)].indexOf(...r)]="y"
+//        palyerStatus=1
+//     }
+//     console.log(x)
+//
+//    if(y.length==0){
+//     break
+//    }
+//
+// }
+
+// let tt="Barev bolorin"
+// function views(txt){
+//     let x=""+txt
+//    return x.length
+//
+// }
+//
+//
+//
+// // views(tt)
+// // views("barev Aziz")
+// let a=views(77)
+//
+// console.log(a)
+
+// function e(arg){
+//     document.write(arg)
+//     return false
+// }
+// console.log(e("Dashery sxal eq lracrel"))
+//
+// function a(dram){
+//     return b(dram)
+// }
+// function b(dram){
+//    return  c(dram+100)
+// }
+// function c(dram){
+//     return dram-200
+// }
+//
+//
+// console.log(a(5000))
+//
+//
+//
+
+//
+// function login(log,pss){
+//     let v=validate(log,pss)
+//
+//     if(v){
+//         home(v)
+//     }
+// }
+// function validate(log,pss){
+//     if(true){
+//        return  action(log,pss)
+//     }else{
+//        return errors("dashtery sxal en")
+//     }
+// }
+// function action(log,pss){
+//     if(false){
+//         return "Valodta"
+//     }else{
+//        return  errors("log pass sxal en")
+//     }
+// }
+// function errors(msg){
+//
+//     console.log(msg)
+//     return false
+// }
+// function home(data){
+//     alert(data)
+// }
+// login(prompt("login"),prompt("password"))
+
+
+
+//
+// let f1=function(arjeq) {
+//
+//     return arjeq+1
+// }
+// function f(a,b) {
+//
+//     return a(b)
+// }
+// console.log(f(f1,48))
+
+
+
+// tnayin 1
+
+// stexcel calculator
+// 3 promptov
+// functionerov
+
+//  tnayin 2
+
+// unenakl prompt vory vori mej kharcni dher
+// tariqy duq petq e haskananq inqy tiv e grel te voch ete che het uxarki avart
+// ete chisht e grel uxarki sery ete vory klini 0 ev 1 ete ay
+// tveric baci ayl tiv grec asum eq hajox
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 3.  promptum , arandznacnel tver inqy gtni ayd tveri vra bajanvox tver minchev 1000 mijayq (50,20,25)
+
+
+
+
+
+
+
+
+
+
