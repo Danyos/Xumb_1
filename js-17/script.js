f9719095ac7180d1c827ad4d4ff90e794e9a475a
Danyos <daniel.hambardzumyan@mail.ru>
2022-12-05 21:03:24 +0400

    recusia arrow function

---
+++ js-17/script.js
@@ -0,0 +1,148 @@
+//naxord dasi tnayin mnum e nuyn@
+
+// function f() {
+//
+// }
+// f()
+
+// function a(params){
+//    return params/2
+//
+// }
+// console.log(x)
+// console.log(f)
+// let arr=[1,4,5,89,564,56,54];
+// //
+// // let y=arr.map(a)
+// // console.log(y)
+//
+// let ab=arr.map(function (arr){
+//
+// })
+
+
+
+
+
+// function ananun(func){
+//     console.log(func(5))
+//
+// }
+//
+//
+// ananun(function (par){
+//     return par*2
+// })
+let myarr=[7,14,87,15,16]
+let x=function (){
+    alert(7)
+}
+let y=()=>{
+    alert(8)
+}
+
+// let x2=function (){
+//     return 7
+// }
+
+// let y2=()=> 7
+// let x3=function (params){
+//     return 7
+// }
+// let y3=params=>params*2
+// console.log(y3())
+
+// let x4=function (arg,arr){
+//     return arg+arr
+// }
+// let y4=(arg,arr)=>arg+arr
+
+// let x5=function (arr){
+//     if(arr){
+//         return 7
+//     }
+//     return  8
+// }
+// v1
+// let y5=arr=>{
+//     if(arr){
+//         return 7
+//     }
+//     return  8
+// }
+// v2
+// let y5=arr=>arr?7:8
+
+
+
+//
+// let exampl=myarr.map(res=>res*2)
+// console.log(exampl)
+
+// *************************REKURSIA*************************
+// function a(){
+//     b()
+// }
+// function b(){
+//   c()
+// }
+// function c(){
+//     a()
+// }
+// a()
+
+// function sum(i=0){
+//
+//     if(i==4){
+//         return i;
+//     }
+//    return  i + sum(i+1)
+//
+// }
+// console.time("sum")
+// console.log(sum())
+// console.timeEnd("sum")
+//
+//
+// function sum2(i=0,sum=0){
+//
+//     if(i==4){
+//         return sum
+//     }
+//     sum+=i
+//     return sum2(i+1,sum)
+// }
+//
+// console.time("sum2")
+// console.log(sum2())
+// console.timeEnd("sum2")
+//
+
+
+
+// tnayin
+// xai tesqov
+
+//1 prompt unenal vor@ kharcni dher qarti gaxtnabary sxal patasxanelu depqum noric kberi prompty kasi sxal es patasxanel
+//2 dnel limit 3 sxal patasxani depqum asi duq kexc ogtater eq
+// 3 erb chisht gres asi greq dher bjjan heraxoshamary eli limiotov
+
+// 4
+// // xndir  function a(arg=0,sum=0){
+//
+// }
+// a(7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
