9315acdf0658ae5f2eaf79bb956e1270b5342e74
Danyos <daniel.hambardzumyan@mail.ru>
2022-12-12 21:02:52 +0400

    rekursia xndirner

---
+++ js-19/script.js
@@ -0,0 +1,80 @@
+
+//1.
+// let test=["Vazgen","muxi","grish"]
+//
+// function games(test,pmsg="greq anun@",i=0){
+//    let test_1= prompt(pmsg)
+//     if(test_1.toUpperCase()==test[i].toUpperCase()){
+//
+//         if(i<test.length-1){
+//             i++
+//             return games(test,test[i],i)
+//         }
+//         return "avart"
+//
+//     }
+//     games(test,pmsg,i)
+// }
+// games(test)
+
+
+//2.
+// function f(a,b=0,sum=0,d=true) {
+//
+//
+//     if(b==0 && a!=0 || a>b && d) {
+//         return f(b,a,sum,false)
+//     }
+//
+//         if(a-1===b){
+//             return sum
+//         }
+//         sum+=b
+//         b--
+//         return  f(a,b,sum,false)
+//
+//
+//
+// }
+// console.log(f(15,7 ))
+// console.log(f(15 ))
+// console.log(f(7,15 ))
+
+
+// tnayin
+
+//1.xndir 2-i nman nuyn kzbunqov. 3 ketov stexcel aynpisi rekursia erb muqtagres
+// tiv veradarcni qez parz tvery zangvac (lucel  rekursia) orinak f(7,15)// 7,11,13 || f(7) // 1,3,57 || f(14,2)// 2,5,7,11,13
+// 2. xndir 2-i nman nuyn kzbunqov. 3 ketov stexcel aynpisi
+// ashxati veradarcni zuygeri zangvac  orinak //f(7,15)//[8,10,12,14]
+
+// 3. zangvac unenal jnjel krknvoxnery andamnery (rekursiayov)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
