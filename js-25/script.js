abf8beff62529cce52360ba8276eb5481ce44a9b
Danyos <daniel.hambardzumyan@mail.ru>
2022-12-26 21:03:50 +0400

    test

---
+++ js-25/script.js
@@ -0,0 +1,39 @@
+// hashvel jamayin grafiky yst roepi
+// petq e promptum grel varkyan hashvi te qani or qani jam qani rope e ayn kazmum
+// orinak erb gres
+// 180600 beri
+// 2 or
+// 2 jam
+// 10 rope
+// ...code
+
+let v=prompt("greq varkyan","180600")
+let d=parseInt(v/3600/24)
+let h=parseInt(v/3600%24)
+let r=Math.round(v/3600%24*60 % 60)
+console.log("or "+ d)
+console.log("jam "+ h)
+console.log("rope "+ r)
+
+
+
+// https://pcvector.net/scripts/animation/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
